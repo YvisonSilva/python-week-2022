7e8f6f4be393d21eebbe05c12bb3a7845941a93d
YvisonSilva <104322109+YvisonSilva@users.noreply.github.com>
2022-04-27 18:06:39 -0300

    Update models.py

---
+++ beerlog/models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
+
+class Beer(SQlModel, table=True):
+    id: Optional [int] = Field(primary_key=True)
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    
+brewdog = Beer(name=Brewdog, style='NEIPA', flavor=6, image=8, cost=8)
