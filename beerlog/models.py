f849a73a2fcea5e0d52544cc1813fb29602b2666
YvisonSilva <104322109+YvisonSilva@users.noreply.github.com>
2022-04-27 18:17:50 -0300

    Update models.py

--- beerlog/models.py
+++ beerlog/models.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Field
 from sqlmodel import select
 
 class Beer(SQlModel, table=True):
-    id: Optional [int] = Field(primary_key=True)
+    id: Optional [int] = Field(primary_key=True, default=Nome, index=True)
     name: str
     style: str
     flavor: int
